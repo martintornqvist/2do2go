4499ecf8880c38479360b56e5371e1a33246fb5d
Martin Törnqvist <martin.tornqvist@schibsted.se>
2016-05-02 15:25:37 +0200

    Added uid generator to fire on click and push it to db instead of fb loooong uid

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -1,14 +1,14 @@
-todoTogo.controller('homeController', ['$scope', '$firebaseArray', 'ngDialog', '$rootScope',
-          function($scope, $firebaseArray, ngDialog, $rootScope){
+todoTogo.controller('homeController', ['$scope', '$firebaseArray', 'ngDialog', '$rootScope', '$routeParams',
+          function($scope, $firebaseArray, ngDialog, $rootScope, $routeParams){
 
             // Getting a Firebase reference
             allLists = new Firebase('https://todotogo.firebaseio.com/lists/');
             //oneList = new Firebase('https://todotogo.firebaseio.com');
 
             // Create function for the modal
-            $scope.create = function(){
+            $scope.createe = function(){
                     // Pushing a new list id to Firebase
-                      lists = allLists.push({
+                      allLists = allLists.push({
                       name: 'new list'
                     });
                       // Fetches the created list UID
@@ -24,4 +24,43 @@ todoTogo.controller('homeController', ['$scope', '$firebaseArray', 'ngDialog', '
                       });
                       console.log('listUID is: ' + listUID);
                     };
+
+
+
+
+
+
+
+
+
+            $scope.create = function(){
+              // Generate a shorter random uid (6 chars) that replaces the long regular Firebase uid
+              letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+              uid_length = 6;
+              generator = function(){
+                random = '';
+                for(var i = 0; i < uid_length; i++){
+                  random += letters.charAt(Math.floor(Math.random() * letters.length));
+                }
+                return random;
+              }
+              generator();
+
+              var lists = new Firebase('https://todotogo.firebaseio.com/lists/' + random);
+              firebaseLists = $firebaseArray(lists);
+
+              lists.child(random).set(random);
+              firebaseLists.$add(random).then(function(lists){
+              })
+
+              // Runs the ngDialog scheme with template etc
+              ngDialog.open({
+                template:'create.html',
+                controller: 'createController',
+                scope: $scope,
+                className:'ngdialog-theme-default'
+              });
+              console.log('listUID is: ' + random);
+            }
+
 }]);
