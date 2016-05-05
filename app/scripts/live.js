669b0ca71d6f5399a92a6653d548e7968e2aad79
Martin Törnqvist <martin.tornqvist@schibsted.se>
2016-05-05 22:32:06 +0200

    viewer is now able to add, view and delete items as well

--- app/scripts/live.js
+++ app/scripts/live.js
@@ -14,6 +14,40 @@ todoTogo.controller('liveController', ['$scope', '$firebaseArray', '$location', 
         // Start with empty array
         $scope.items = $firebaseArray(addItems);
 
+        $scope.addItem = function(){
+            var timestamp = new Date().valueOf();
+            $scope.items.$add({
+                id: timestamp,
+                name: $scope.itemInput,
+                done: false
+            });
+            $scope.itemInput = '';
+        },
+
+        // Check if todoInput-field is empty, if not, run addItem function
+        $scope.addPost = function(){
+            if(event.keyCode == 13 && $scope.itemInput != ""){
+                $scope.addItem();
+            }
+        },
+
+        // Remove item from scope
+        $scope.removeItem = function(index, item){
+            if(item.id === undefined)
+            return;
+            $scope.items.$remove(item);
+        },
+
+        // Edit item in scope and save to Firebase
+        $scope.editMode = function(){
+          $(event.target).closest('li').toggleClass('editing');
+        },
+        $scope.editOnEnter = function(item){
+          if(event.keyCode == 13 && item.name){
+            $scope.editMode();
+            $scope.items.$save(item);
+          }
+        };
 
 }]);
 
