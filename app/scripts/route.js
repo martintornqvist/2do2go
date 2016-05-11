7e2b1e6eb549045bdfe23db182b517130e372dc1
Martin Törnqvist <martin.tornqvist@schibsted.se>
2016-05-11 22:07:11 +0200

    route changes for deploy

--- app/scripts/route.js
+++ app/scripts/route.js
@@ -4,35 +4,32 @@ todoTogo.config(['$routeProvider', '$locationProvider', function($routeProvider,
 
   // Route for landing page
   .when('/home', {
-    templateUrl: '/home.html',
+    templateUrl: './home.html',
     controller: 'homeController'
   })
 
   .when('/home/:random', {
-    templateUrl: '/create.html',
+    templateUrl: './create.html',
     controller: 'liveController'
   })
 
   // Route for FAQ page
   .when('/faq', {
-    templateUrl: '/faq.html'
+    templateUrl: './faq.html'
   })
 
   // Route for create page
   .when('/create', {
-    templateUrl: '/create.html',
+    templateUrl: './create.html',
     controller: 'createController'
   })
 
   // Route for live view
   .when('/live/:random', {
-    templateUrl: '/live.html',
+    templateUrl: './live.html',
     controller: 'liveController'
   })
 
   .otherwise({ redirectTo: '/home' });
-
-  // No hashbang
-  //$locationProvider.html5Mode(true);
 
 }]);
