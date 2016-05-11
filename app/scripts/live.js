2a8a4f4de479e6ee2a329f07f2405af6a3f5eaba
Martin Törnqvist <martin.tornqvist@schibsted.se>
2016-05-11 22:06:43 +0200

    support for add-click on live view

--- app/scripts/live.js
+++ app/scripts/live.js
@@ -1,14 +1,11 @@
 todoTogo.controller('liveController', ['$scope', '$firebaseArray', '$location', '$firebaseObject', 'ngDialog', '$rootScope', '$routeParams',
       function($scope, $firebaseArray, $rootScope, $location, $firebaseObject, ngDialog, $routeParams){
         var ref = new Firebase('https://todotogo.firebaseio.com');
-        // Showing all lists that user created, display better!!!
-        // TEMP TO SEE IF IT WORKS
+
         var allLists = new Firebase('https://todotogo.firebaseio.com/lists/' + $routeParams.random);
         $scope.listCounter = $firebaseArray(allLists);
 
         // Function for adding new items to parent list at Firebase
-        // Add items to list
-        // TEMP TO SEE IF IT WORKS
         var addItems = new Firebase('//todotogo.firebaseio.com/lists/' + $routeParams.random);
 
         // Start with empty array
@@ -29,6 +26,11 @@ todoTogo.controller('liveController', ['$scope', '$firebaseArray', '$location', 
             if(event.keyCode == 13 && $scope.itemInput != ""){
                 $scope.addItem();
             }
+        },
+        $scope.addPostButton = function(){
+          if($scope.itemInput != ""){
+            $scope.addItem();
+          }
         },
 
         // Remove item from scope
